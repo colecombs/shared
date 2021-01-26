8eb3e6a6da84c6a744efc74e3db6de281190f15f
colecombs <cole.b.combs@gmail.com>
2021-01-25 20:59:57 -0600

    Add files via upload

---
+++ pillow.py
@@ -0,0 +1,13 @@
+
+# using pillow for image manipulation PIL == Python Imaging Library
+
+
+
+
+
+# Opening an image
+
+#from PIL import Image
+
+#dock_img = Image.open('dock.jpg')
+#dock_img.show()
